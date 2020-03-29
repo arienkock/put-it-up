8d5283613891c2c67e214b341b216b6ca21697dc
Arien Kock <arien@positor.nl>
2020-03-29 18:33:27 +0200

    Refactor code, animate remote sticky moves

--- render-to-dom.js
+++ render-to-dom.js
@@ -2,55 +2,63 @@
 // TODO: Use pointer events to support pinch zoom gesture
 // TODO: Have fixed zoom scale in an array of constants
 // TODO: Show an indicator of where something will be snapped to
+// TODO: Move multiple stickies at once
+// TODO: Add a sticky without a keyboard
+// TODO: Move a sticky without a keyboard
+// TODO: Delete sticky
 
 const STICKY_TYPE = "application/sticky";
 const DEFAULT_STICKY_COLOR = "khaki";
 const zoomScale = [0.5, 1];
-export function mount(board, element) {
-  const renderSticky = (id, sticky) => {
+
+export function mount(board, domElement) {
+  let stickyMovedLocally = null;
+  const renderSticky = (stickyId, sticky) => {
     const container = getStickyElement(
-      element,
-      id,
+      domElement,
+      stickyId,
       board.updateText,
       board.getStickyLocation
     );
-    const { sticky: stickyElement, inputElement } = container;
-    container.style.left = sticky.location.x * element.boardScale + "px";
-    container.style.top = sticky.location.y * element.boardScale + "px";
-    inputElement.style.display = element.boardScale < 0.6 ? "none" : "block";
-    const size = 100 * element.boardScale + "px";
-    container.style.width = size;
-    container.style.height = size;
-    stickyElement.style.padding = 10 * element.boardScale + "px";
-    container.style.padding = 5 * element.boardScale + "px";
-    stickyElement.style.backgroundColor = sticky.color || DEFAULT_STICKY_COLOR;
-    if (inputElement.value !== sticky.text) {
-      inputElement.value = sticky.text;
-      fitContentInSticky(stickyElement, inputElement);
+    const shouldAnimateMove = stickyMovedLocally !== stickyId
+    setStickyStyles(
+      sticky,
+      container,
+      domElement.boardScale,
+      shouldAnimateMove
+    );
+    if (shouldAnimateMove) {
+      stickyMovedLocally = null
+    }
+    const textarea = container.inputElement;
+    if (textarea.value !== sticky.text) {
+      textarea.value = sticky.text;
+      fitContentInSticky(container.sticky, textarea);
     }
   };
   const render = () => {
-    element.boardScale = element.boardScale || zoomScale[zoomScale.length - 1];
-    const size = 8000 * element.boardScale + "px";
-    element.style.width = size;
-    element.style.height = size;
-    element.style.transform = `scale(${element.boardScale})`;
+    domElement.boardScale =
+      domElement.boardScale || zoomScale[zoomScale.length - 1];
+    const size = 8000 * domElement.boardScale + "px";
+    domElement.style.width = size;
+    domElement.style.height = size;
+    domElement.style.transform = `scale(${domElement.boardScale})`;
     Object.entries(board.getState().stickies).forEach(entry => {
       const [id, sticky] = entry;
       renderSticky(id, sticky);
     });
   };
 
-  element.ondragover = event => {
+  domElement.ondragover = event => {
     event.preventDefault();
   };
 
-  element.ondrop = event => {
+  domElement.ondrop = event => {
     const { clientX: x, clientY: y } = event;
     const { id, originalLocation, dragStart } = JSON.parse(
       event.dataTransfer.getData(STICKY_TYPE)
     );
-    const squareScale = element.boardScale * element.boardScale;
+    const squareScale = domElement.boardScale * domElement.boardScale;
     const offset = {
       x: (x - dragStart.x) / squareScale,
       y: (y - dragStart.y) / squareScale
@@ -59,22 +67,23 @@ export function mount(board, element) {
       x: originalLocation.x + offset.x,
       y: originalLocation.y + offset.y
     };
+    stickyMovedLocally = id;
     board.moveSticky(id, newLocation);
   };
   let nextClickCreatesNewSticky = false;
   document.body.onkeyup = event => {
     if (event.key === "o") {
-      let index = zoomScale.findIndex(v => v === element.boardScale) + 1;
-      element.boardScale = zoomScale[index % zoomScale.length];
-      render(board, element);
+      let index = zoomScale.findIndex(v => v === domElement.boardScale) + 1;
+      domElement.boardScale = zoomScale[index % zoomScale.length];
+      render(board, domElement);
     } else if (event.key === "n") {
       nextClickCreatesNewSticky = true;
     }
   };
-  element.onclick = event => {
+  domElement.onclick = event => {
     if (nextClickCreatesNewSticky) {
       nextClickCreatesNewSticky = false;
-      const rect = element.getBoundingClientRect();
+      const rect = domElement.getBoundingClientRect();
       const location = {
         x: event.clientX - rect.left - 50,
         y: event.clientY - rect.top - 50
@@ -100,16 +109,9 @@ function getStickyElement(boardElement, id, updateTextById, getStickyLocation) {
   const stickyIdClass = "sticky-" + id;
   let container = boardElement[stickyIdClass];
   if (!container) {
-    container = document.createElement("div");
+    container = createStickyContainerDOM(stickyIdClass);
     boardElement[stickyIdClass] = container;
-    container.innerHTML =
-      '<div class="sticky"><textarea class="textInput text" rows="1" tabindex="-1"></textarea></div>';
     boardElement.appendChild(container);
-    container.classList.add(stickyIdClass);
-    container.inputElement = container.querySelector(".textInput");
-    container.sticky = container.querySelector(".sticky");
-    container.classList.add("sticky-container");
-    container.sticky.setAttribute("draggable", "true");
     container.ondragstart = event => {
       const { clientX: x, clientY: y } = event;
       const originalLocation = getStickyLocation(id);
@@ -146,15 +148,14 @@ function getStickyElement(boardElement, id, updateTextById, getStickyLocation) {
 }
 
 function fitContentInSticky(sticky, textarea) {
-  console.log("fitting for " + textarea.value, new Error());
   textarea.rows = 1;
   textarea.style.fontSize = "1.5rem";
   let fontSize = 1.5;
-  const wsMatches = textarea.value.match(/\s+/g);
-  const numWhitespaces = wsMatches === null ? 0 : wsMatches.length;
+  const wordMatches = textarea.value.match(/\S+/g);
+  const numWords = wordMatches === null ? 0 : wordMatches.length;
   while (true) {
     let adjusted = false;
-    if (textarea.rows < numWhitespaces || textarea.value.length > 15) {
+    if (textarea.rows < numWords || textarea.value.length > 15) {
       if (
         textarea.scrollHeight > textarea.clientHeight &&
         sticky.scrollHeight <= sticky.clientHeight
@@ -169,11 +170,41 @@ function fitContentInSticky(sticky, textarea) {
     }
     if (textarea.scrollHeight > textarea.clientHeight && fontSize > 0.5) {
       adjusted = true;
-      fontSize -= 0.1;
+      fontSize -= 0.25;
       textarea.style.fontSize = fontSize + "rem";
     }
     if (!adjusted) {
       break;
     }
   }
+}
+
+function setStickyStyles(sticky, container, scaleFactor, animateMove) {
+  const { sticky: stickyElement, inputElement } = container;
+  if (animateMove) {
+    container.style.transition = "left 1s, top 1s";
+  } else {
+    container.style.transition = "none";
+  }
+  container.style.left = sticky.location.x * scaleFactor + "px";
+  container.style.top = sticky.location.y * scaleFactor + "px";
+  inputElement.style.display = scaleFactor < 0.6 ? "none" : "block";
+  const size = 100 * scaleFactor + "px";
+  container.style.width = size;
+  container.style.height = size;
+  stickyElement.style.padding = 10 * scaleFactor + "px";
+  container.style.padding = 5 * scaleFactor + "px";
+  stickyElement.style.backgroundColor = sticky.color || DEFAULT_STICKY_COLOR;
+}
+
+function createStickyContainerDOM(stickyIdClass) {
+  const container = document.createElement("div");
+  container.innerHTML =
+    '<div class="sticky"><textarea class="textInput text" rows="1" tabindex="-1"></textarea></div>';
+  container.classList.add(stickyIdClass);
+  container.inputElement = container.querySelector(".textInput");
+  container.sticky = container.querySelector(".sticky");
+  container.classList.add("sticky-container");
+  container.sticky.setAttribute("draggable", "true");
+  return container;
 }
