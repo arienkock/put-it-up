94e4524fed42d0b1809cb52c534759ecc695ae8f
Arien Kock <akock@worth.systems>
2020-06-27 18:27:13 +0200

    Made certain board properties 'private'

--- src/board.js
+++ src/board.js
@@ -1,70 +1,70 @@
-function Board(boardId, db) {
+function Board(boardId, dbArg) {
   let name = "";
-  this.items = {};
+  const items = {};
   Object.defineProperty(this, "boardId", {
     value: boardId,
     writable: false,
   });
-  this.boardListeners = [];
-  this.itemListeners = [];
-  this.idGen = 0;
-  this.db = db;
+  const boardListeners = [];
+  const itemListeners = [];
+  let idGen = 0;
+  const db = dbArg;
   this.connect = () => {
-    if (this.db) {
-      const boardRef = this.db.collection("boards").doc(this.boardId);
-      boardRef.onSnapshot((doc) => this.handleBoardSnapshot(doc));
+    if (db) {
+      const boardRef = db.collection("boards").doc(this.boardId);
+      boardRef.onSnapshot((snapshot) => handleBoardSnapshot(snapshot));
       const itemsRef = boardRef.collection("items");
-      itemsRef.onSnapshot((items) => this.handleItemsSnapshot(items));
+      itemsRef.onSnapshot((snapshot) => handleItemsSnapshot(snapshot));
     }
   };
   itemsRef = () =>
-    this.db.collection("boards").doc(this.boardId).collection("items");
+    db.collection("boards").doc(this.boardId).collection("items");
   this.getName = () => name;
   this.setName = (newName) => {
-    if (this.db) {
-      this.db.collection("boards").doc(this.boardId).update({ name: newName });
+    if (db) {
+      db.collection("boards").doc(this.boardId).update({ name: newName });
     }
     name = newName;
   };
   this.getItems = () => {
-    return this.items;
+    return items;
   };
   this.hold = (item, boundingRectangle) => {
     const data = { item, boundingRectangle };
     let id;
-    if (this.db) {
+    if (db) {
       const docRef = itemsRef().doc();
       docRef.set(data);
       id = docRef.id;
     } else {
-      id = ++this.idGen;
+      id = ++idGen;
     }
-    this.items[id] = data;
-    this.itemListeners.forEach((fn) => fn(data));
+    items[id] = data;
+    itemListeners.forEach((fn) => fn(data));
     return id;
   };
   this.getItem = (id) => {
-    return this.items[id];
+    return items[id];
   };
   this.removeItem = (id) => {
-    if (this.db) {
+    if (db) {
       itemsRef().doc(id).delete();
     }
-    delete this.items[id];
-    this.itemListeners.forEach((fn) => fn(undefined));
+    delete items[id];
+    itemListeners.forEach((fn) => fn(undefined));
   };
   this.moveItem = (id, boundingRectangle) => {
-    if (this.db) {
+    if (db) {
       itemsRef().doc(id).update({ boundingRectangle });
     }
-    this.items[id].boundingRectangle = boundingRectangle;
+    items[id].boundingRectangle = boundingRectangle;
   };
   this.getSize = () => {
     let maxBottom = 0,
       minTop = 0,
       maxRight = 0,
       minLeft = 0;
-    Object.values(this.items).forEach((item) => {
+    Object.values(items).forEach((item) => {
       maxBottom = Math.max(maxBottom, item.boundingRectangle.bottom);
       maxRight = Math.max(maxRight, item.boundingRectangle.right);
       minTop = Math.min(minTop, item.boundingRectangle.top);
@@ -78,25 +78,25 @@ function Board(boardId, db) {
     };
   };
   this.addListener = (boardListener, itemListener) => {
-    this.boardListeners.push(boardListener);
-    this.itemListeners.push(itemListener);
+    boardListeners.push(boardListener);
+    itemListeners.push(itemListener);
   };
-  this.handleBoardSnapshot = (boardSnapshot) => {
+  function handleBoardSnapshot(boardSnapshot) {
     const boardData = boardSnapshot.data();
     name = boardData.name;
-  };
-  this.handleItemsSnapshot = (itemsSnapshot) => {
+  }
+  function handleItemsSnapshot(itemsSnapshot) {
     itemsSnapshot.docChanges().forEach((change) => {
       if (change.type === "added" || change.type === "modified") {
         const data = change.doc.data();
-        this.items[change.doc.id] = data;
-        this.itemListeners.forEach((fn) => fn(data));
+        items[change.doc.id] = data;
+        itemListeners.forEach((fn) => fn(data));
       } else {
-        delete this.items[change.doc.id];
-        this.itemListeners.forEach((fn) => fn(undefined));
+        delete items[change.doc.id];
+        itemListeners.forEach((fn) => fn(undefined));
       }
     });
-  };
+  }
   this.connect();
 }
 
