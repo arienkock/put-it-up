4e675cbd25c13539d8a6693bf2309f36f1c238ab
Arien Kock <arien.kock@gmail.com>
2025-11-08 16:35:40 +0100

    chore: Update Jest configuration for improved coverage reporting
    Modified jest.config.js to enhance coverage collection by specifying files to include and exclude, updating the coverage directory, and adding various coverage reporters. Additionally, added new npm scripts in package.json for running tests with coverage and watch modes, streamlining the testing process.

--- jest.config.js
+++ jest.config.js
@@ -19,24 +19,39 @@ module.exports = {
   // Reduced timeout for faster test execution
   testTimeout: 30000, // 10 seconds
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  // collectCoverage: false, // Set via CLI flag or npm script
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "scripts/**/*.js",
+    "!scripts/**/*.spec.js",
+    "!scripts/**/*.test.js",
+    "!scripts/**/__tests__/**",
+    "!scripts/**/__mocks__/**",
+    "!scripts/config/firebase-config.js", // Config file, not testable
+  ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: ".nyc_output",
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/test/",
+    "/\\.nyc_output/",
+    "/coverage/",
+    "\\.spec\\.js$",
+    "\\.test\\.js$",
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  // ],
+  coverageReporters: [
+    "text",
+    "text-summary",
+    "html",
+    "json",
+    "lcov",
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
